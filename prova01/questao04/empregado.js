dc3b75f6eaa86f61fa23a24f77ce32d19068004a
icomp <luan.alencar@icomp.ufam.edu.br>
2023-09-13 09:31:35 -0400

    adicionada questao 4

---
+++ prova01/questao04/empregado.js
@@ -0,0 +1,41 @@
+class empregado{
+    constructor(nome, salario){
+        this.nome = nome
+        this.salario = salario
+    }
+
+    getSalario(){
+        return this.salario
+    }
+
+    setSalario(salario){
+        if(salario < 0){
+            console.log("ERRO. Número negativo inserido. Salário não poôde ser atualizado.")
+        }
+        else{
+            this.salario = salario
+        }
+    }
+
+    static somaSalarios(empregados){
+        let tamanho = empregados.length
+        let soma = 0.00
+        for(let i = 0; i < tamanho; i++){
+            soma = soma +  empregados[i].getSalario()
+        }
+        return soma;
+    }
+}
+
+emp1 = new empregado("Jorge", 1500.00)
+
+emp2 = new empregado("Amanda", 2000.00)
+
+console.log(emp1.getSalario())
+console.log(emp2.getSalario())
+
+emp1.setSalario(1650.00)
+emp2.setSalario(2200.00)
+
+console.log(emp1.getSalario())
+console.log(emp2.getSalario())
