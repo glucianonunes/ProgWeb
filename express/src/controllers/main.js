6df48b569680acfc3ee8e63d37d0d9e07bafd987
icomp <g.luciano.nunes@gmail.com>
2023-10-04 09:48:00 -0400

    a

---
+++ express/src/controllers/main.js
@@ -0,0 +1,16 @@
+// Arquivo src/controllers/main.js
+const index = (req, res) => {
+    const conteudo = 'Página principal da aplicação';
+    res.render('main/index', {
+        conteudo,
+    });
+};
+
+const sobre = (req, res) => {
+    const conteudo = 'Página sobre a aplicação';
+    res.render('main/sobre', {
+        conteudo,
+    });
+};
+
+module.exports = { index, sobre }
--- express/src/index.js
+++ express/src/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
-const router = require("../config/routes")
+const router = require("../src/router/router")
 const logger = require("morgan") // Morgan é um middleware usado para registros de logs
 const handlebars = require('express-handlebars');
+const sass = require('node-sass-middleware');
+
 
 const app = express();
+
+
+
+app.use(sass({
+src: `${__dirname}/../public/scss`,
+dest: `${__dirname}/../public/css`,
+outputStyle: "compressed",
+prefix: "/css",
+}));
+app.use("/css", express.static(`${__dirname}/../public/css`));
 
 const PORT = 3456;
 
 
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
+//  app.use(express.static(`public/img`));
 app.set("views", `${__dirname}/views`);
+
+app.use('/img', [
+  express.static(`${__dirname}/public/img`)
+  ]);
+
 
 app.use(router)
 
